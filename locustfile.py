c2f620073ed3af947e2371ef9defef7b1ee5aa17
Totsaporn Siripokkul <trophy.tot@gmail.com>
2019-12-02 16:20:30 +0700

    Create locustfile.py

---
+++ locustfile.py
@@ -0,0 +1,26 @@
+from locust import HttpLocust, TaskSet, task, between
+
+class WebsiteTasks(TaskSet):
+    # def on_start(self):
+    #     self.client.post("/login", {
+    #         "username": "test_user",
+    #         "password": ""
+    #     })
+    
+    @task
+    def post_message(self):
+        self.client.post("/v1/message", data={
+        "message": "Hello"
+    })
+
+    # @task
+    # def index(self):
+    #     self.client.get("/health")
+        
+    # @task
+    # def about(self):
+    #     self.client.get("/about/")
+
+class WebsiteUser(HttpLocust):
+    task_set = WebsiteTasks
+    wait_time = between(5, 15)
